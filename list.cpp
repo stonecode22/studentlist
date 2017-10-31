f4331c22301f84aa35a260841efc7ac251d08683
U-BSD\389737 <389737@SHS-10DCE4735.beaverton.k12.or.us>
2017-10-31 12:21:31 -0700

    fix print function

--- list.cpp
+++ list.cpp
@@ -1,63 +1,104 @@
 /*Student List by Stone Yang */
 #include<iostream>
-#include<string>
+#include<string.h>
 #include<vector>
+#include<iomanip>
 using namespace std;
 
 struct person
 {
-  char fname[12];
-  char lname[12];
+  char fname[20];
+  char lname[20];
   int id;
   float gpa;
 };
 
-person &p = p;
-person add(person&p);
-person print(person&p);
+//programminghelporg for explaining how to use vectors, referred by Alex Y.
+person add(vector<person> &p, char fname[12], char lname[12], int id, float gpa);
+person del(vector<person> &p, int id);
+person print(vector<person> &p);
+vector<person> p;
 
 int main()
 {
-  char cmd;
-  char loop;
-  do
-    {
-      cout << "What would you like to do?" << endl;
-      cout << "Type 1 to create a new entry, 2 to print student list, and 3 to delete an entry." << endl;
-      cin >> cmd;
-      if(cmd = '1')
-	{
-	  add(A);
-	}
-      else if(cmd = '2')
-	{
-	  print(A);
-	}
-      
-      cout << "Do you wish to make more changes? (y/n)" << endl;
-      cin >> loop;
-    }while(loop != 'n');
-    return 0;
-}
-
-person add(person&p)
-{
-  cout << "Enter first name: ";
-  cin >> p.fname;
-  cout << endl << "Enter last name: ";
-  cin >> p.lname;
-  cout << endl << "Enter ID: ";
-  cin >> p.id;
-  cout << endl << "Enter GPA: ";
-  cin >> p.gpa;
-  cout << p.fname << " " << p.lname << "'s Profile Saved." << endl;
-  return p;
+  char choice[20];
   
+  cout << "Type ADD to create a new entry, PRINT to print student list, DELETE to delete an entry, and EXIT to exit." << endl;
+  cin >> choice;
+  
+    if(strcmp(choice, "ADD") == 0) //add a student
+      {
+	char fname[20];
+	char lname[20];
+	int id;
+	float gpa;
+	
+	cout << "Enter first name: ";
+	cin >> fname;
+	cout << endl << "Enter last name: ";
+	cin >> lname;
+	cout << endl << "Enter ID: ";
+	cin >> id;
+	cout << endl << "GPA: ";
+	cin >> gpa;
+	add(p, fname, lname, id, gpa);
+	cout << "Student profile saved." << endl;
+	main();
+      }
+    else if (strcmp(choice, "DELETE") == 0) //delete a student
+      {
+	int id;
+	
+	cout << "Student ID Number to delete: ";
+	cin >> id;
+	del(p, id);
+	cout << "Student profile deleted." << endl;
+	main();
+      }
+    else if (strcmp(choice, "PRINT") == 0) //print students
+      {
+        print(p);
+	main();
+      }
+    else if (strcmp(choice, "EXIT") == 0) //quit program
+      {
+	return 0;
+      }
+    else //if none of commands are typed
+      {
+	cout << "Invalid command. Try again." << endl;
+	main();
+      }
 }
 
-person print(person&p)
+person add(vector<person> &p, char fname[20], char lname[20], int id, float gpa)
 {
-  cout << p.fname << " " << p.lname << endl;
-  cout << "ID: " << p.id << endl;
-  cout << "GPA: " << p.gpa << endl;
+  p.push_back(person()); //Credit programminghelporg
+  int i = p.size() - 1;
+  
+  strcpy(p[i].fname, fname);
+  strcpy(p[i].lname, lname);
+  p[i].id = id;
+  p[i].gpa = gpa;
+}
+
+person del(vector<person> &p, int id)
+{
+  for(int i = 0; i < p.size(); i++)
+    {
+      if(p[i].id == id)
+	{
+	  p.erase(p.begin() + i);
+	}
+    }
+}
+
+person print(vector<person> &p)
+{
+  vector<person>::iterator it;
+  for(it = p.begin(); it != p.end(); ++it)
+    {
+      std::cout << *it << endl;
+      //cout << p[*it].fname << " " << p[*it].lname << endl << " ID: " << p[*it].id << endl << setprecision(2) << " GPA: " << p[*it].gpa << endl;
+    }
 }
